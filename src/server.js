1df225b4897162043f03e0f09c4f17fe6c1de618
Aaron <amsegal93@gmail.com>
2020-05-22 05:22:34 -0400

    Fixing small typo

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app')
 const {PORT} = require('./config')
 
 app.listen(PORT, () => {
-    console.log(`Server listening at ${POR}`)
+    console.log(`Server listening at ${PORT}`)
 })
