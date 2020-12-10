4b6aea1228232b2a42256c5104df5d0e932aecb7
Aaron <amsegal93@gmail.com>
2020-12-10 11:52:02 -0500

    Testing changes to fix heroku crash

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('../.node_modules/dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const knex = require('knex')
+const knex = require('../.node_modules/knex')
 const app = require('./app')
 const {PORT, DATABASE_URL} = require('./config')
 
