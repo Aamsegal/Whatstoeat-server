7e80790967583447f2f9fd67f2606aeece40598d
Aaron <amsegal93@gmail.com>
2020-12-24 04:07:09 -0500

    Fixed coding standard issues found

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
 const WTE_userRouter = require('../endPoints/users/WTE_users-router');
 const WTE_recipeRouter = require('../endPoints/recipe/WTE_recipe-router');
 const WTE_ingredientRouter = require('../endPoints/ingredients/WTE_ingredient-router');
 const WTE_allergyRouter = require('../endPoints/allergies/WTE_allergies-router');
-const app = express()
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
     : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Whats to eat server is running')
+    res.send('Whats to eat server is running');
 })
 
 app.use('/api/userEndpoint', WTE_userRouter);
